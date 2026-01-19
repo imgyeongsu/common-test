b2e5c432050f0035f6bb7c1d351cbe0e7436c997
임경수 <05dos1211@gmail.com>
2026-01-19 14:32:08 +0900

    진짜 구축 테스트 전 정리

--- src/features/webrtc-chat/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * WebRTC Chat Feature - Public API
- *
- * 외부에서 사용할 요소만 export (캡슐화)
- */
-
-export { useWebRTCChat } from './hooks/useWebRTCChat';
-export { ChatRoom } from './components/ChatRoom';
